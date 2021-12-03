a36ecfc305b95b8713b3c6090b1c9a775cdb1126
mrjacobsullivan <mrjacobsullivan@gmail.com>
2021-12-03 11:59:57 -0600

    added [GET] /api/projects endpoint

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,30 @@
-// Write your "projects" router here!
+const express = require('express');
+
+// const {} = require('./projects-middleware');
+
+const Projects = require('./projects-model');
+
+// BASE - /api/projects
+const router = express.Router();
+
+// [GET] /api/projects
+router.get('/', async (req, res, next) => {
+  try {
+    const projects = await Projects.get();
+    res.json(projects);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// [GET] /api/projects/:id
+
+// [POST] /api/projects
+
+// [PUT] /api/projects/:id
+
+// [DELETE] /api/projects/:id
+
+// [GET] /api/projects/:id/actions
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const projectsRouter = require('./projects/projects-router');
+
 const { logger, errorHandling } = require('./global-middleware');
 
 const server = express();
@@ -7,10 +9,7 @@ const server = express();
 server.use(express.json());
 server.use(logger);
 
-// Configure your server here
-// Build your actions router in /api/actions/actions-router.js
-// Build your projects router in /api/projects/projects-router.js
-// Do NOT `server.listen()` inside this file!
+server.use('/api/projects', projectsRouter);
 
 server.use(errorHandling);
 
