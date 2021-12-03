ad8ef4eaa3a2856db0d50e439fcb98cdea2c0f02
mrjacobsullivan <mrjacobsullivan@gmail.com>
2021-12-03 13:50:16 -0600

    added [POST] /api/actions endpoint

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -15,6 +15,27 @@ const validateActionId = async (req, res, next) => {
   }
 };
 
+const validateAction = (req, res, next) => {
+  /*
+    project_id: required
+    description: required, up to 128 in length
+    notes: required
+    completed: not required, defaults to false
+  */
+  const { project_id, description, notes, completed } = req.body;
+
+  if (!project_id || !description || description.length > 128 || !notes) {
+    next({
+      status: 400,
+      message:
+        'Project Id, description, and notes are required. Description must be 128 characters or less.',
+    });
+  } else {
+    next();
+  }
+};
+
 module.exports = {
   validateActionId,
+  validateAction,
 };
--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const { validateActionId } = require('./actions-middlware');
+const { validateActionId, validateAction } = require('./actions-middlware');
 
 const Actions = require('./actions-model');
 
@@ -27,6 +27,23 @@ router.get('/:id', validateActionId, async (req, res, next) => {
 });
 
 // [POST] /api/actions
+router.post('/', validateAction, async (req, res, next) => {
+  try {
+    // project_id: required
+    // description: required, up to 128 in length
+    // notes: required
+    // completed: not required, defaults to false
+    const action = await Actions.insert({
+      project_id: req.body.project_id,
+      description: req.body.description,
+      notes: req.body.notes,
+      completed: req.body.completed,
+    });
+    res.status(201).json(action);
+  } catch (err) {
+    next(err);
+  }
+});
 
 // [PUT] /api/actions/:id
 
